e7fac3adb080670c7e723c62f56367859386f53d
forrest-keyper <ppaisley94@gmail.com>
2019-10-15 23:26:01 -0700

    continued commenting and function stubbing. last commit for 10/15/2019

--- app.py
+++ app.py
@@ -8,8 +8,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def splash():
-    return 'Heoo World!'
+    return 'Hello World!'
 
+
+'''
+
+@app.route('/register')
+def register():
+
+    # will eventually need to check for user unique identity
+    # accept user form input for username and email (password later)
+    
+    # what does this return statement need to be for the confirmation function?
+    return('/templates/dashboard.html')
+
+@app.route('/register/confirm')
+def confirmRegistration():
+
+    # confirm user creation, move to dashboard
+    return('templates/dashboard.html')
+
+@app.route('/dashboard')
+def dashboard('/dashboard')
+
+    # checks for user log in, takes user id and displays associated trackers
+    #
+    return('/templates/dashboard.html')
+
+
+'''
 
 if __name__ == "__main__":
     app.run(debug=True)
