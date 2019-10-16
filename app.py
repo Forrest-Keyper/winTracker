5916c633d9963bc5e339560c7ff1cb071f98ca20
forrest-keyper <ppaisley94@gmail.com>
2019-10-15 21:48:24 -0700

    flask hello world operation

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 '''
 
 '''
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def splash():
+    return 'Heoo World!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
