3e5563ab0e6ab0f3c4aed9fb0a3d46868c2c156a
Parker M Paisley <ppaisley94@gmail.com>
2019-10-16 18:20:36 -0700

    plenty of changes. Currently hosts index with buttons to home and register. register button redirects.

--- app.py
+++ app.py
@@ -2,26 +2,36 @@
 
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bson.objectid import ObjectId
+from pymongo import MongoClient
 app = Flask(__name__)
 
 
 @app.route('/')
-def splash():
-    return 'Hello World!'
+# home page, will expand in following commits
+def index():
+    return render_template('index.html')
+
+
+
+
+@app.route('/register')
+# registration page, sign up link should redirect here
+def register():
+    user = {
+        'name': request.form.get('username'),
+        'password': request.form.get('password')
+    }
+    # will eventually need to check for user unique identity
+    # accept user form input for username and email (password later)
+
+    # what does this return statement need to be for the confirmation function?
+    return render_template('register.html')
 
 
 '''
-
-@app.route('/register')
-def register():
-
-    # will eventually need to check for user unique identity
-    # accept user form input for username and email (password later)
-    
-    # what does this return statement need to be for the confirmation function?
-    return('/templates/dashboard.html')
-
+@app.route('/user')
 @app.route('/register/confirm')
 def confirmRegistration():
 
